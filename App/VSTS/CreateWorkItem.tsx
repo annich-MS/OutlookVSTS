27746ace178d74f6ac411d70a1e30632f0c0dc02
Emily Zhou <t-emzhou@microsoft.com>
2016-08-09 12:46:33 -0700

    removing office.init in createworkitem

--- App/VSTS/CreateWorkItem.tsx
+++ App/VSTS/CreateWorkItem.tsx
@@ -13,37 +13,20 @@ import { Gear } from './Gear';
  */
 
 export class CreateWorkItem extends React.Component<{}, {}> {
-
-  // public isReady: boolean; // set to false
-
-  /*private Initialize(): void {
-    this.isReady = true;
-    this.forceUpdate(); // re-renders page
-  }
-
-  public constructor() { // will be removed after merge since office is initialized before this page is reached
-    super();
-    this.isReady = false;
-    this.Initialize = this.Initialize.bind(this);
-    Office.initialize = this.Initialize;
-  }*/
-
-/**
- * Renders the div that contains all the components of the Create page
- */
-public render(): React.ReactElement<{}> {
-  console.log('got to create pg');
- /* if (this.isReady === false) {
-    return (<div>loading!!</div>);
-  }*/
-
-  return (<div>
-  <Gear />
-  <WorkItemDropdown/>
-  <Title/>
-  <Description/>
-  <Classification/>
-  <Save/>
-  </div>);
+  /**
+   * Renders the div that contains all the components of the Create page
+   */
+  public render(): React.ReactElement<{}> {
+    console.log('got to create pg');
+    return (
+      <div>
+        <Gear />
+        <WorkItemDropdown/>
+        <Title/>
+        <Description/>
+        <Classification/>
+        <Save/>
+      </div>
+    );
   }
 }
