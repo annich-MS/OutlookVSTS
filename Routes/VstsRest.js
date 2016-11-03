5e2c70c43e8fabd4ac193471602c90def1e9c80d
annich-ms <annich@microsoft.com>
2016-11-03 16:04:35 -0700

    remove translate

--- Routes/VstsRest.js
+++ Routes/VstsRest.js
@@ -250,8 +250,6 @@ router.getMessage = function (req, res) {
 router.use('/getMessage', router.getMessage);
 
 function downloadMessageFromEWS(messageId, ewsUrl, token, callback) {
-  messageId = messageId.replace(/_/g, '+');
-  messageId = messageId.replace(/-/g, '/'); 
   var body = '<?xml version="1.0" encoding="utf-8"?>' +
     '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
     '               xmlns:xsd="http://www.w3.org/2001/XMLSchema"' +
